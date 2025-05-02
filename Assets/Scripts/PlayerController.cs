42d1b230c745cbc310ad92199863a90a5122aa75
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-02 11:36:45 +0200

    third_person_player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
